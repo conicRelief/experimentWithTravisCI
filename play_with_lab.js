aa3da2ccd2749859797a9042b69a4593ebbcf9c7
Otto Wagner <owagner@nectarom.com>
2016-03-17 12:42:18 -0500

    Add tests for the purpose of trying out Travis CI.

---
+++ play_with_lab.js
@@ -0,0 +1,24 @@
+const Code = require('code');
+const Lab = require('lab');
+
+const lab = exports.lab = Lab.script();
+
+lab.test("Returns True when 1 + 1 = 2", done =>{
+	Code.expect(1 + 1).to.equal(2);
+	done();
+});
+
+lab.test("Returns True when 1 + 1 = 2", done =>{
+	Code.expect(1 + 1).to.equal(2);
+	done();
+});
+
+lab.test("Returns True when 1 + 1 = 2", done =>{
+	Code.expect(1 + 1).to.equal(2);
+	done();
+});
+
+lab.test("", done =>{
+	Code.expect(true).to.equal(true);
+	done();
+});
